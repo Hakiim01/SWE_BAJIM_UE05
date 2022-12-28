66931a00f3fd6cdc5843dcf1a2abce9055524db2
Hakim Bajim <bajimhakim123@gmail.com>
2022-12-28 22:29:23 +0100

    Initial commit

--- tests/test_partlists_t.cpp
+++ tests/test_partlists_t.cpp
@@ -4,17 +4,33 @@
 
 #include "../content/partlist/partlist_t.h"
 
-void test_partlists_t() {
+void test_partlists_t_creation_of_combined_part() {
+    std::cout << "\ntest_partlists_t_creation_of_combined_part: \n";
+    partslists::composite_part_t chair("chair");
     partslists::part_t leg("leg");
     partslists::part_t sittingspace("sittingspace");
-    partslists::composite_part_t stool("chair");
-    stool.add_part(leg);
-    stool.add_part(leg);
-    stool.add_part(leg);
-    stool.add_part(leg);
-    stool.add_part(sittingspace);
+    chair.add_part(leg);
+    chair.add_part(leg);
+    chair.add_part(leg);
+    chair.add_part(leg);
+    chair.add_part(sittingspace);
     formatter::set_formatter s;
-    s.print_parts(stool);
+    s.print_parts(chair);
     formatter::hierarchy_formatter h;
-    h.print_parts(stool);
+    h.print_parts(chair);
+}
+
+void test_partlists_t_creation_of_part() {
+    std::cout << "\ntest_partlists_t_creation_of_part: \n";
+    partslists::part_t leg("leg");
+    partslists::part_t sittingspace("sittingspace");
+    formatter::set_formatter s;
+    s.print_parts(leg);
+    formatter::hierarchy_formatter h;
+    h.print_parts(leg);
+}
+
+void test_partlists_t() {
+    test_partlists_t_creation_of_part();
+    test_partlists_t_creation_of_combined_part();
 }
