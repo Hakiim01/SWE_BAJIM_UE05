0cfaea073dcc33ded9ae17f3c7f6e3402a036d39
Hakim Bajim <bajimhakim123@gmail.com>
2022-12-28 17:52:20 +0100

    Initial commit

--- content/partlist/partlist_t.cpp
+++ content/partlist/partlist_t.cpp
@@ -2,7 +2,60 @@
 // Created by Hakim Bajim on 28.12.22.
 //
 
+#include <map>
 #include "partlist_t.h"
 
 namespace partslists {
+
+    part_t::part_t(const std::string &name) {
+        this->name = name;
+    }
+
+    std::string part_t::get_name() const {
+        return this->name;
+    }
+
+    bool part_t::equals(const part_t &part) const {
+        return this->name == part.name;
+    }
+
+
+    composite_part_t::composite_part_t(const std::string &name) : part_t(name) {
+        this->name = name;
+    }
+
+    void composite_part_t::add_part(part_t &part) {
+        this->parts.push_back(&part);
+    }
+
+    std::vector<part_t *> composite_part_t::get_parts() {
+        return this->parts;
+    }
+
 } // partslists
+
+namespace formatter {
+
+    void hierarchy_formatter::print_parts(partslists::part_t &part, std::ostream &out) {
+        out << part.get_name();
+    }
+
+    void set_formatter::print_parts(partslists::part_t &part, std::ostream &out) {
+        std::map<std::string, int> m;
+        fill_map(part, out, m);
+        auto *comp = dynamic_cast<partslists::composite_part_t *>(&part);
+        out << comp->get_parts().size() << " " << comp->get_name();
+    }
+
+    void set_formatter::fill_map(partslists::part_t &part, std::ostream &out, std::map<std::string, int> &m) {
+        auto *comp = dynamic_cast<partslists::composite_part_t *>(&part);
+        if(comp->get_parts().size() > 0) {
+            for(auto &i : comp->get_parts()) {
+                fill_map(*i, out, m);
+            }
+        } else {
+            m.contains(comp->get_name()) ? ++m[comp->get_name()] : m[comp->get_name()] = 1;
+        }
+    }
+
+} // formatter
--- content/partlist/partlist_t.h
+++ content/partlist/partlist_t.h
@@ -5,12 +5,59 @@
 #ifndef SWE_BAJIM_UE05_PARTLIST_T_H
 #define SWE_BAJIM_UE05_PARTLIST_T_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 namespace partslists {
 
-    class partlist_t {
+    class part_t {
+    protected:
+        std::string name;
 
+    public:
+        explicit part_t(const std::string &name);
+
+        virtual ~part_t() = default;
+
+        [[nodiscard]] std::string get_name() const;
+
+        [[nodiscard]] bool equals(const part_t &part) const;
+    };
+
+    class composite_part_t : public part_t{
+    protected:
+        std::vector<part_t*> parts = {};
+
+    public:
+        explicit composite_part_t(const std::string &name);
+
+        void add_part(part_t &part);
+
+        std::vector<part_t*> get_parts();
     };
 
 } // partslists
+
+namespace formatter{
+    class formatter_t {
+    public:
+        virtual void print_parts(partslists::part_t &part, std::ostream &out = std::cout) = 0;
+    };
+
+    class set_formatter : public formatter_t {
+    private:
+        void fill_map(partslists::part_t &part, std::ostream &out, std::map<std::string, int> &m);
+    public:
+        void print_parts(partslists::part_t &part, std::ostream &out = std::cout) override;
+    };
+
+    class hierarchy_formatter : public formatter_t {
+    private:
+        void print_parts_rec(partslists::part_t &part, std::ostream &out = std::cout, int indent = 1);
+    public:
+        void print_parts(partslists::part_t &part, std::ostream &out = std::cout) override;
+    };
+} // formatter
 
 #endif //SWE_BAJIM_UE05_PARTLIST_T_H
